55cd05a80e8202b9db8075648679af5317ac200c
Snoobinoob <cheeseycheese99@gmail.com>
2023-03-30 20:44:01 -0400

    Initial commit

---
+++ src/main/java/livesplitHooks/LivesplitHooksEntry.java
@@ -0,0 +1,11 @@
+package livesplitHooks;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class LivesplitHooksEntry {
+
+    public void init() {
+        System.out.println("Hello world from LiveSplit Hooks!");
+    }
+}
